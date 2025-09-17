41a1f8752bf460287480179a9b8e587e2db085bd
paris <hwangseok.paris@gmail.com>
2025-09-17 17:50:49 +0900

    250917: ImageView, Intent

---
+++ app/src/main/java/com/example/kotlin_study_1/EplLogo1.kt
@@ -0,0 +1,16 @@
+package com.example.kotlin_study_1
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class EplLogo1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_epl_logo1)
+
+
+    }
+}
--- app/src/main/java/com/example/kotlin_study_1/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_study_1/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.kotlin_study_1
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,5 +14,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val ars_image = findViewById<ImageView >(R.id.ars)
+        var liv_image = findViewById<ImageView>(R.id.liv)
+        ars_image.setOnClickListener {
+            Toast.makeText(this, "아스날 우승!!", Toast.LENGTH_LONG).show()
+
+        val intent = Intent(this, EplLogo1::class.java)
+            startActivity(intent)
+
+
+        liv_image.setOnClickListener {
+            Toast.makeText(this, "리버풀 3위!!!", Toast.LENGTH_LONG).show()
+
+            val intent = Intent(this, EplLogo2::class.java)
+            startActivity(intent)
+        }
+        }
     }
 }
